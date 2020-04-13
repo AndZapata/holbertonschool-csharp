df16acc9050296b30e2d66358f3b1932c0f50067
AndZapata <andzapata514@gmail.com>
2020-04-13 21:30:31 +0000

    [Add] Print 3 times a sentences and the first 9 chars

---
+++ 0x00-csharp-hello_world/6-print_string/6-print_string.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string str = "Holberton School";
+	for (int i = 0; i < 4; i++)
+	{
+	    if (i < 3)
+	    {
+                Console.Write("{0}", str);
+            }
+	    else if (i == 3)
+	    {
+	        Console.Write("\n{0}\n", str.Substring(0, 9));
+            }
+        }
+    }
+}
