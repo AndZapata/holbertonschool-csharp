8c9b1082db7d9ec0072ca70a25e026dbaa6d013d
AndZapata <andzapata514@gmail.com>
2020-04-14 19:37:38 +0000

    [Add] alphabet in lowercase

---
+++ 0x01-csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+            Console.Write("{0}", letter);
+        }
+    }
+}
