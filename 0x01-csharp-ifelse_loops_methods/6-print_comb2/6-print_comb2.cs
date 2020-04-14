aa9d805f8646be2e595b6eef3e34af354a568076
AndZapata <andzapata514@gmail.com>
2020-04-14 20:48:24 +0000

    [Add] No equal numbers

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int i, j;
+	for (i = 0; i <= 8; i++)
+	{
+	    for (j = 1; j <= 9; j++)
+	    {
+	        if (i < j)
+		{
+		    if (i == 8 && j == 9)
+		    {
+		        Console.Write("{0}{1}\n", i, j);
+                    }
+		    else
+		    {
+		        Console.Write("{0}{1}, ", i, j);
+                    }
+                }
+            }
+        }
+    }
+}
