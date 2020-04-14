24232b79291828f3b9f46f0e39a3573b242c57fd
AndZapata <andzapata514@gmail.com>
2020-04-14 21:51:09 +0000

    [Add] print char n times

---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintLine(int length)
+    {
+        if (length <= 0)
+	{
+	    Console.Write("\n");
+        }
+	else
+	{
+	    string ch = new String('_', length);
+	    Console.WriteLine(ch);
+        }
+    }
+}
