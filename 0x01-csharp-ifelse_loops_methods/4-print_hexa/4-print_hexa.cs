e1f249315089be061e8b63b76afc4a5e6f36f1d7
AndZapata <andzapata514@gmail.com>
2020-04-14 20:00:20 +0000

    [Add] print number decimal and hexa

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int number;
+        for (number = 0; number < 99; number++)
+	{
+	    Console.WriteLine("{0} = 0x{0:x}", number);
+        }
+    }
+}
