3aaf2fb3ee6c4a1deea7130207f58046bcc9e291
AndZapata <andzapata514@gmail.com>
2020-04-14 21:26:46 +0000

    [Add] last digit

---
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        int mod;
+	if (number > 0)
+	{
+	    mod = number % 10;
+        }
+	else
+	{
+	    mod = ((-1 * number) % 10);
+        }
+	Console.Write(mod);
+	return (mod);
+    }
+}
