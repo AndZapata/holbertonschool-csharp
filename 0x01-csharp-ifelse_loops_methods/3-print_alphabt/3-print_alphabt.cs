6377430ffd0c46908c7fc62339cf8baa6b5d924a
AndZapata <andzapata514@gmail.com>
2020-04-14 19:48:46 +0000

    [Add] Alphabet less q and e

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+	    if (letter != 'e' && letter != 'q')
+	    {
+	        Console.Write(letter);
+            }
+        }
+    }
+}
