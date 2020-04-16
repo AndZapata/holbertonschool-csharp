e0e85c1659344046c4a3e7061e50f91c51d5694e
AndZapata <andzapata514@gmail.com>
2020-04-16 17:07:54 +0000

    [Add] append integers in an array

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        int i;
+	int[] index;
+	if (size < 0)
+	{
+	    Console.WriteLine("Size cannot be negative");
+            return (null);
+        }
+	index = new int[size];
+        for (i = 0; i < size; i++)
+	{
+            index[i] = i;
+        }
+        return (index);
+    }
+}
