d32c9bbc6bb376eaa0b812daa9dafd54d44e6ee2
AndZapata <andzapata514@gmail.com>
2020-04-16 19:33:26 +0000

    [Add] list created

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        int i;
+        if (size < 0)
+	{
+	    Console.WriteLine("Size cannot be negative");
+	    return null;
+        }
+	List<int> newList = new List<int>();
+	if (size == 0)
+	{
+	    Console.Write("\n");
+	    return newList;
+        }
+	for (i = 0; i < size; i++)
+	{
+	    newList.Add(i);
+	    if (i < size - 1)
+	        Console.Write("{0} ", i);
+	    else
+	        Console.Write("{0}\n", i);
+        }
+        return newList;
+    }
+}
