1702724f3b56a778929f38cfdfb486b1f00b5431
AndZapata <andzapata514@gmail.com>
2020-04-17 00:34:57 +0000

    [Add] dictionary best score

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        if (myList.Count == 0)
+	    return "None";
+        List<string> newList = new List<string>();
+        foreach (KeyValuePair<string, int> item in myList.OrderByDescending(key => key.Value))
+	{
+	    newList.Add(item.Key);
+        }
+        return newList[0];
+    }
+}
