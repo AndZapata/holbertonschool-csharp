c2f1aa5a5bbe34c6c893248f98546f44c7251cb1
AndZapata <andzapata514@gmail.com>
2020-04-17 00:59:42 +0000

    [Add] matriz

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int i, j, num = 0;
+	for (i = 0; i < 5; i++)
+	{
+	    for (j = 0; j < 5; j++)
+	    {
+	        if (j == 2 && i == 2)
+		    Console.Write(num + 1);
+		else
+	            Console.Write(num);
+            }
+	    Console.WriteLine();
+        }
+    }
+}
