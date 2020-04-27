c7bf13d1b625b02c7a0e014ee46d282446d9107a
AndZapata <andzapata514@gmail.com>
2020-04-27 18:42:00 -0500

    Update 3-stack_push_pop.cs

--- 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -5,19 +5,15 @@ class MyStack
 {
     public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
     {
+        bool flag = aStack.Contains(search);
         Console.WriteLine("Number of items: {0}", aStack.Count);
         if (aStack.Count > 0)
             Console.WriteLine("Top item: {0}", aStack.Peek());
         else
             Console.WriteLine("Stack is empty");
-        if (aStack.Contains(search))
-        {
-            Console.WriteLine("Stack contains {0}: {1}", search, "True");
-            while (aStack.Pop() != search);
-        }
-        else
-            Console.WriteLine("Stack contains {0}: {1}", search, "False");
-        
+        Console.WriteLine("Stack contains {0}: {1}", search, flag);
+        if (flag)
+            while (aStack.Pop() != search);        
         aStack.Push(newItem);
         
         return aStack;
