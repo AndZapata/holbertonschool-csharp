2e2bc6dc353e9db460479d317cda73685bfb35e0
AndZapata <andzapata514@gmail.com>
2020-04-27 14:10:10 -0500

    [Add] script for building a csharp project

---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash                                                        
+  dotnet new console -n $1                                                   
+  cd $1                                                                      
+  mv Program.cs $1.cs                                                        
