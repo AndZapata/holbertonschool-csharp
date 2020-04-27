2a99980195da83f0bb60d62dc411e320ffa25ed6
AndZapata <andzapata514@gmail.com>
2020-04-27 14:33:15 -0500

    [Add] program to add all unique integers

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        List<int> unique = myList.Distinct().ToList();
+        int total = unique.Sum(item => item);
+        return total;
+    }
+}
