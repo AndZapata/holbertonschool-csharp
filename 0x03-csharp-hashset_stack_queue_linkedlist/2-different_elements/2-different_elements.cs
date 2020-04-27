963103343956b7092dbec82ffb7d830afe187bb2
AndZapata <andzapata514@gmail.com>
2020-04-27 15:44:54 -0500

    [Add] Unique elements in two lists

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        List<int> diff = new List<int>();
+        foreach (int i in list1)
+            if (!list2.Contains(i))
+                diff.Add(i);
+
+        foreach (int j in list2)
+            if (!list1.Contains(j))
+                diff.Add(j);
+
+        diff.Sort();                
+        return diff;
+    }
+}
