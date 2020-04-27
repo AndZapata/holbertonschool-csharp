5c9b073101ecd30031bf2a222392dfd496df5dcc
AndZapata <andzapata514@gmail.com>
2020-04-27 18:15:24 -0500

    [Add] full stack

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        if (aStack.Count > 0)
+        {
+            Console.WriteLine("Number of items: {0}\nTop item: {1}", aStack.Count, aStack.Peek());
+            Console.WriteLine("Stack contains {0}: {1}", search, aStack.Contains(search) ? "True" : "False");
+            while (aStack.Pop() != search);
+            aStack.Push(newItem);
+        }
+        else
+            Console.WriteLine("Stack is empty");
+
+
+        return aStack;
+    }
+}
