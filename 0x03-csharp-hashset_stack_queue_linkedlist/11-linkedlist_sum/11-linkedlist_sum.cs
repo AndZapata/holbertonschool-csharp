7ddc3a155bc271beaa9dd43b186dfce8ffc5d59c
AndZapata <andzapata514@gmail.com>
2020-04-27 21:46:43 -0500

    [Add] sum all elements in a Linked list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Sum(LinkedList<int> myLList)
+    {
+        int result = 0;
+        foreach (int i in myLList)
+            result += i;
+        return result;
+    }
+}
