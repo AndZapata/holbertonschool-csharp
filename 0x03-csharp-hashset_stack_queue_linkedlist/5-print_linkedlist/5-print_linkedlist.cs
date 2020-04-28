e546e547bd53809918dcf9d7ddf79f055a45db67
AndZapata <andzapata514@gmail.com>
2020-04-27 19:34:54 -0500

    [Add] create a linkedList of a given size

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> link = new LinkedList<int>();
+
+        if (size > 0)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                link.AddFirst(i);
+                Console.WriteLine(i);
+            }
+        }
+
+        return link;
+    }
+}
