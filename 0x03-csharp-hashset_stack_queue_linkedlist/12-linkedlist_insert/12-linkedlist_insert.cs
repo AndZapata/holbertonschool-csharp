df639419a6c6d4ece588311cfd6a0c71fcbfa7dd
AndZapata <andzapata514@gmail.com>
2020-04-27 23:48:00 -0500

    Update 12-linkedlist_insert.cs

--- 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -5,14 +5,14 @@ class LList
 {
     public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
     {
-        LinkedListNode<int> current = myLList.First;
         LinkedListNode<int> newNode = new LinkedListNode<int>(n);
 
-        if (current == null || myLList.First.Value >= newNode.Value)
+        if (myLList.First == null || myLList.First.Value >= newNode.Value)
         {
             myLList.AddLast(newNode);
             return newNode;
         }
+        LinkedListNode<int> current = myLList.First;
         while (current.Next != null && current.Next.Value < newNode.Value)
             current = current.Next;
         myLList.AddAfter(current, newNode);
