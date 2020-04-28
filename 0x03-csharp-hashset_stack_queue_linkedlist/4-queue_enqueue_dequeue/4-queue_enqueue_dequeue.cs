816126401b54ef497a5c9102183c64346b8d860a
AndZapata <andzapata514@gmail.com>
2020-04-27 19:21:14 -0500

    [Add] full Queue

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        int len = aQueue.Count;
+        bool check = aQueue.Contains(search);
+        Console.WriteLine("Number of items: {0}", len);
+        if (len > 0)
+            Console.WriteLine("First item: {0}", aQueue.Peek());
+        else
+            Console.WriteLine("Queue is empty");
+        aQueue.Enqueue(newItem);
+        Console.WriteLine("Queue contains {0}: {1}", search, check);
+        if (check)
+            while(aQueue.Dequeue() != search);
+        return aQueue;
+    }
+}
