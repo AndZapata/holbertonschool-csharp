24ddd81b58b5ca0e51473e6394fee5fdb0536bc0
AndZapata <andzapata514@gmail.com>
2020-04-27 22:59:55 -0500

    Update 13-linkedlist_delete.cs

--- 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -5,12 +5,15 @@ class LList
 {
     public static void Delete(LinkedList<int> myLList, int index)
     {
-        LinkedListNode<int> current = myLList.First;
-        for (int i = 0; i <= index && current.Next != null; i++)
+        if (myLList.Count > 0)
         {
-            if (i == index)
-                myLList.Remove(current);
-            current = current.Next;
+            LinkedListNode<int> current = myLList.First;
+            for (int i = 0; i <= index && current.Next != null; i++)
+            {
+                if (i == index)
+                    myLList.Remove(current);
+                current = current.Next;
+            }
         }
     }
 }
