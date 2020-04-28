58cec0a3174fb192454db2e800356669f43865b6
AndZapata <andzapata514@gmail.com>
2020-04-28 14:27:08 -0500

    [Fix] Add Try catch inside For loop, dividing two lists

--- 0x04-csharp-exceptions/2-divide_lists/2-divide_lists.cs
+++ 0x04-csharp-exceptions/2-divide_lists/2-divide_lists.cs
@@ -5,6 +5,23 @@ class List
 {
     public static List<int> Divide(List<int> list1, List<int> list2, int listLength)
     {
-        Console.WriteLine("Hello World!");
+        List<int> div = new List<int>();
+        for (int i = 0; i < listLength; i++)
+        {
+            try
+            {
+                div.Add(list1[i] / list2[i]);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Cannot divide by zero");
+                div.Add(0);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Out of range");
+            }
+        }
+        return div;
     }
 }
