e038029013fbc66f63573fa4ccd537a358ffcce8
AndZapata <andzapata514@gmail.com>
2020-04-27 20:31:57 -0500

    [Add] find a given value and returns its specific index

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        int idx = 0;
+        
+        foreach (int i in myLList)
+        {
+            if (i == value)
+                return idx;
+            idx += 1;
+        }
+        return -1;
+    }
+}
