9463a44923c11b548b19bf573616a5adee72bc22
AndZapata <andzapata514@gmail.com>
2020-04-27 23:46:03 -0500

    [Fix] insert node in a sorted linked list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> current = myLList.First;
+        LinkedListNode<int> newNode = new LinkedListNode<int>(n);
+
+        if (current == null || myLList.First.Value >= newNode.Value)
+        {
+            myLList.AddLast(newNode);
+            return newNode;
+        }
+        while (current.Next != null && current.Next.Value < newNode.Value)
+            current = current.Next;
+        myLList.AddAfter(current, newNode);
+        return newNode;
+    }
+}
