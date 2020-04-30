04ce77ed8ce3b1542b5d4975da4fded2f32bc977
AndZapata <andzapata514@gmail.com>
2020-04-30 16:43:48 -0500

    [Add] File descriptor

--- run.sh
+++ run.sh
@@ -2,3 +2,5 @@
   dotnet new console -n $1                                                   
   cd $1                                                                      
   mv Program.cs $1.cs                                                        
+  sed -i '6  a \\t\t\<DocumentationFile\>bin\\\$(Configuration)\\\$(TargetFramework)\\\$(AssemblyName).xml\<\/DocumentationFile\>' $1.csproj
+  
