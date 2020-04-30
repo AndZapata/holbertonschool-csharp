053dfc22b1e5f7ce72a93e41b4d66dea4743b8c3
AndZapata <andzapata514@gmail.com>
2020-04-30 17:32:38 -0500

    [Add] Override string

---
+++ 0x06-csharp-classes/5-enemy/5-enemy.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>This class create a Zombie</summary>
+    public class Zombie
+    {
+        /// <summary>How many healt has the zombie</summary>
+        private int health;
+        private string name = "(No name)";
+        /// <summary>Constructor for the healt of the zombie</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+        /// <summary>Constructor that checks the value</summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            health = value;
+        }
+        /// <summary>public property Name</summary>
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+        /// <summary>public method, returns the value of the healt</summary>
+        public int GetHealth()
+        {
+            return health;
+        }
+        /// <summary>public method toString() for overring the string object</summary>
+        public override string ToString() => $"Zombie name: {Name} / Total Health: {health}";
+    }
+}
