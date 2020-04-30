fb8a9d5f8a47284abd802f4074862ac48f03c133
AndZapata <andzapata514@gmail.com>
2020-04-30 16:59:04 -0500

    [Add] adding public field healt for the zombie

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>This class create a Zombie</summary>
+    public class Zombie
+    {
+        /// <summary>How many healt has the zombie</summary>
+        public int health;
+        /// <summary>Constructor for the healt of the zombie</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}
