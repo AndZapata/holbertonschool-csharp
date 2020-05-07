4564e107ea5f2f86dcf870bc90bc5469da53f6fe
AndZapata <andzapata514@gmail.com>
2020-05-07 16:10:13 -0500

    [Add] first commit

---
+++ 0x08-csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/// <summary>
+/// Class Shape
+/// </summary>
+class Shape
+{
+    /// <summary>
+    /// Function that throws a error massage exception
+    /// </summary>
+    /// <returns>new throws message exception</returns>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
