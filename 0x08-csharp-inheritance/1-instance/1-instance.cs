7e2711b1da633f1a7a2c3167bebbaedaf0250559
AndZapata <andzapata514@gmail.com>
2020-05-07 14:44:21 -0500

    [Add] First Commit

---
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/// <summary>
+/// Class Obj
+/// </summary>
+class Obj
+{
+    /// <summary>
+    /// Function to check if an object is instance of array
+    /// </summary>
+    /// <param name="obj">The object to be checked</param>
+    /// <returns>true if is an instance of array, false if not</returns>
+    public static bool IsInstanceOfArray(object obj)
+    {
+        return typeof(Array).IsInstanceOfType(obj);
+    }
+}
