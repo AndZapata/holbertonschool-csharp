03075a3b20f066a3e950c2ff21ecad01ca54ff46
AndZapata <andzapata514@gmail.com>
2020-05-07 15:51:59 -0500

    [Add First comment]

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+
+class Obj
+{
+    public static void Print(object myObj)
+    {
+        TypeInfo tInfo = myObj.GetType().GetTypeInfo();
+        Console.WriteLine("{0} Properties:", tInfo.Name);
+        foreach (PropertyInfo prop in tInfo.GetProperties())
+            Console.WriteLine("{0}", prop.Name);
+        Console.WriteLine("{0} Methods:", tInfo.Name);
+        foreach (MethodInfo meth in tInfo.GetMethods())
+            Console.WriteLine("{0}", meth.Name);
+    }
+}
