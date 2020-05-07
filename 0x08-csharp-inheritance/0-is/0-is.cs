9c01e9709f4432b488728ec9f79ced57adebd7a0
AndZapata <andzapata514@gmail.com>
2020-05-07 14:32:29 -0500

    [Add] First commit

---
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/// <summary>
+/// Class obj
+/// </summary>
+class Obj
+{
+    /// <summary>
+    /// Function to check if an object is an int
+    /// </summary>
+    /// <param name="obj">the object to be checked</param>
+    /// <returns>true if is an int, otherwise false</returns>
+    public static bool IsOfTypeInt(object obj)
+    {
+        return obj is int;
+    }
+}
