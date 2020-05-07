258b87ac8549b6a78b11c815869f0e190d2d051d
AndZapata <andzapata514@gmail.com>
2020-05-07 14:58:42 -0500

    [Add] First commit

---
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/// <summary>
+/// Class Obj
+/// </summary>
+class Obj
+{
+    /// <summary>
+    /// Function to check if a subclass of a base class
+    /// </summary>
+    /// <param name="derivedType">Object to be checked</param>
+    /// <param name="baseType">Object to compare</param>
+    /// <returns>true if is a subclass, false if not</returns>
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        return derivedType.IsSubclassOf(baseType);
+    }
+}
