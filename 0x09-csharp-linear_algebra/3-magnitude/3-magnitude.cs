11d4dfece36822b604b5eac6dc2777199d3ab288
andzapata <andzapata514@gmail.com>
2020-05-27 02:04:55 -0500

    [Add] Function to check the magnitude of a vector

---
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class VectorMath
+{
+    public static double Magnitude(double[] vector)
+    {
+        if (vector.Length < 2 || vector.Length > 3)
+            return (-1);
+        double result = 0;
+        for (int i = 0; i < vector.Length; i++)
+            result = result + (vector[i] * vector[i]);
+        return (Math.Round(Math.Sqrt(result)));
+    }
+}
