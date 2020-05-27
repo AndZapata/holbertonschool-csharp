36bf7c03a1627792bac83b8044a3c34850aee3fa
andzapata <andzapata514@gmail.com>
2020-05-27 16:51:35 -0500

    [Add] Dot product of 2D 3D vectors

---
+++ 0x09-csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class VectorMath
+{
+    public static double DotProduct(double[] vector1, double[] vector2)
+    {
+        double fail = -1;
+        if ((vector1.Length == 2 || vector1.Length == 3) && (vector2.Length == 2 || vector2.Length == 3))
+        {
+            if (vector1.Length == vector2.Length)
+            {
+                double result = 0;
+                for (int i = 0; i < vector1.Length && i < vector2.Length; i++)
+                    result = result + (vector1[i] * vector2[i]);
+                return result;
+            }
+        }
+        return (fail);
+    }
+}
