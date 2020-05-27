158217a44c2ee9686cd6976e001064f15e7f71e8
andzapata <andzapata514@gmail.com>
2020-05-27 02:10:49 -0500

    Update 3-magnitude.cs

--- 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -9,7 +9,7 @@ class VectorMath
             double result = 0;
             for (int i = 0; i < vector.Length; i++)
                 result = result + (vector[i] * vector[i]);
-            return (Math.Round(Math.Sqrt(result)));
+            return (Math.Round(Math.Sqrt(result), 2));
         }
         return (-1);
     }
