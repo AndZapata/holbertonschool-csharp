5f4d520ca758e5635075567968f75d2f2f556f5f
andzapata <andzapata514@gmail.com>
2020-05-27 02:09:12 -0500

    Update 3-magnitude.cs

--- 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -4,11 +4,13 @@ class VectorMath
 {
     public static double Magnitude(double[] vector)
     {
-        if (vector.Length < 2 && vector.Length > 3)
-            return (-1);
-        double result = 0;
-        for (int i = 0; i < vector.Length; i++)
-            result = result + (vector[i] * vector[i]);
-        return (Math.Round(Math.Sqrt(result)));
+        if (vector.Length == 2 || vector.Length == 3)
+        {
+            double result = 0;
+            for (int i = 0; i < vector.Length; i++)
+                result = result + (vector[i] * vector[i]);
+            return (Math.Round(Math.Sqrt(result)));
+        }
+        return (-1);
     }
 }
