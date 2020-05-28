2d7a9834e3aa9d4b30591d5c100571b8d7ba4c12
andzapata <andzapata514@gmail.com>
2020-05-28 00:18:25 -0500

    [Add] Transpose a matrix

---
+++ 0x09-csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class MatrixMath
+{
+    public static double[,] Transpose(double[,] matrix)
+    {
+        double[,] empty = new double[,] { { } };
+        double[,] result;
+        if (matrix.GetLength(0) == 0)
+            return empty;
+        result = new double[matrix.GetLength(0),matrix.GetLength(1)];
+        for (int i = 0; i < 2; i++)
+            for (int j = 0; j < 2; j++)
+                result[i, j] = matrix[j, i];
+        
+        return result;
+    }
+}
