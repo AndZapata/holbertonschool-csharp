7b9815f058bb70765838f04b5544e94352503db1
andzapata <andzapata514@gmail.com>
2020-05-27 19:34:31 -0500

    Update 14-matrix_addition.cs

--- 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -9,9 +9,9 @@ class MatrixMath
         {
             if (matrix1.GetLength(0) == matrix2.GetLength(0))
             {
-                double[,] result = new double[matrix1.GetLength(0),matrix1.GetLength(0)];
+                double[,] result = new double[matrix1.GetLength(0), matrix1.GetLength(1)];
                 for (int i = 0; i < matrix1.GetLength(0); i++)
-                    for (int j = 0; j < matrix1.GetLength(0); j++)
+                    for (int j = 0; j < matrix1.GetLength(1); j++)
                         result[i, j] = matrix1[i, j] + matrix2[i, j];
                 return result;
             }
