6c83fba62a52a9214ca6dda35ffd0271657ab640
andzapata <andzapata514@gmail.com>
2020-05-27 22:17:17 -0500

    Update 18-matrix_matrix_mul.cs

--- 0x09-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
+++ 0x09-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -4,6 +4,9 @@ class MatrixMath
 {
     public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
     {
+        double[,] fail = new double[,] { { -1 } };
+        if (matrix1.GetLength(0) != matrix2.GetLength(1))
+            return fail;
         double[,] result = new double[matrix1.GetLength(0), matrix1.GetLength(1)];
         double operations = 0;
         for (int i = 0; i < matrix1.GetLength(0); i++)
