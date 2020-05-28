bf8ddb43725727c6a07d42f7180bdcb4a6275641
andzapata <andzapata514@gmail.com>
2020-05-27 22:08:39 -0500

    [Add] mutiply two matrix

---
+++ 0x09-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class MatrixMath
+{
+    public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
+    {
+        double[,] result = new double[matrix1.GetLength(0), matrix1.GetLength(1)];
+        double operations = 0;
+        for (int i = 0; i < matrix1.GetLength(0); i++)
+            for (int j = 0; j < matrix2.GetLength(1); j++)
+            {
+                operations = 0;
+                for (int k = 0; k < matrix1.GetLength(1); k++)
+                    operations = operations + (matrix1[i, k] * matrix2[k, j]);
+                result[i, j] = operations;
+            }
+        return result;
+    }
+}
