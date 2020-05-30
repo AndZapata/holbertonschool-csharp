ed63f2c495baf808849ce34fbc345e6fe48a6cf4
andzapata <andzapata514@gmail.com>
2020-05-29 20:44:28 -0500

    [Add] Inverse of a 2D matrix

---
+++ 0x09-csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class MatrixMath
+{
+    public static double[,] Inverse2D(double[,] matrix)
+    {
+        double divDet = 1 / Determinant(matrix);
+        double [,] fail = new double[,] { { -1 } };
+        double [,] result = new double[2, 2];
+        double [,] trasn = Transpose(matrix);
+        
+        if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
+            return fail;
+        for (int i = 0; i < 2; i++)
+            for (int j = 0; j < 2; j++)
+                result[i, j] = Math.Round(divDet * trasn[i, j], 2);
+        return result;
+    }
+
+    public static double Determinant(double[,] matrix)
+    {
+        double result = 0;
+        if (matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2)
+        {
+            result = Math.Round((matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0]), 2);
+            return result;
+        }
+        return -1;
+    }
+    
+    public static double[,] Transpose(double[,] matrix)
+    {
+        double[,] empty = new double[,] { { } };
+        double[,] result;
+        if (matrix.GetLength(0) == 0)
+            return empty;
+        result = new double[matrix.GetLength(1), matrix.GetLength(0)];
+        result[0, 0] = matrix[1, 1];
+        result[0, 1] = -1 * matrix[0, 1];
+        result[1, 0] = -1 * matrix[1, 0];
+        result[1, 1] = matrix[0, 0];
+        
+        return result;
+    }
+}
