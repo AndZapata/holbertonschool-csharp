9585bfadc1124dfdb6acb4f538f6d5ef38f22d88
andzapata <andzapata514@gmail.com>
2020-05-29 21:03:12 -0500

    Update 33-inverse_2D.cs

--- 0x09-csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
+++ 0x09-csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
@@ -12,56 +12,24 @@ class MatrixMath
     /// <returns>new matrix</returns>
     public static double[,] Inverse2D(double[,] matrix)
     {
-        double det = Determinant(matrix);
+        double det;
         double [,] fail = new double[,] { { -1 } };
         double [,] result = new double[2, 2];
-        double [,] trasn = Transpose(matrix);
+        double [,] trans = new double[2, 2];
         
-        if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2 || det == 0)
+        if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
             return fail;
-        else
-        {
-            double divDet = 1 / det;
-            for (int i = 0; i < 2; i++)
-                for (int j = 0; j < 2; j++)
-                    result[i, j] = Math.Round(divDet * trasn[i, j], 2);
-            return result;
-        }
-    }
-
-    /// <summary>
-    /// Function to obtain the determinant of a 2D matrix
-    /// </summary>
-    /// <param name="matrix"></param>
-    /// <returns>double type determinant</returns>
-    public static double Determinant(double[,] matrix)
-    {
-        double result = 0;
-        if (matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2)
-        {
-            result = Math.Round((matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0]), 2);
-            return result;
-        }
-        return -1;
-    }
-    
-    /// <summary>
-    /// Function to transpose a matrix
-    /// </summary>
-    /// <param name="matrix"></param>
-    /// <returns>new matrix</returns>
-    public static double[,] Transpose(double[,] matrix)
-    {
-        double[,] empty = new double[,] { { } };
-        double[,] result;
-        if (matrix.GetLength(0) == 0)
-            return empty;
-        result = new double[matrix.GetLength(1), matrix.GetLength(0)];
-        result[0, 0] = matrix[1, 1];
-        result[0, 1] = -1 * matrix[0, 1];
-        result[1, 0] = -1 * matrix[1, 0];
-        result[1, 1] = matrix[0, 0];
-        
+        trans[0, 0] = matrix[1, 1];
+        trans[0, 1] = -1 * matrix[0, 1];
+        trans[1, 0] = -1 * matrix[1, 0];
+        trans[1, 1] = matrix[0, 0];
+        det = Math.Round((matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0]), 2);
+        if (det == 0)
+            return fail;
+        double divDet = 1 / det;
+        for (int i = 0; i < 2; i++)
+            for (int j = 0; j < 2; j++)
+                result[i, j] = Math.Round(divDet * trans[i, j], 2);
         return result;
     }
 }
