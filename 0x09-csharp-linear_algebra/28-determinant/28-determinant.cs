26452d3b6cdacf299417be64f8adbd51d5eda589
andzapata <andzapata514@gmail.com>
2020-05-29 19:59:14 -0500

    Update 28-determinant.cs

--- 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
+++ 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -8,9 +8,9 @@ class MatrixMath
         if ((matrix.GetLength(0) == 3 && matrix.GetLength(1) == 3) || (matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2))
         {
             if (matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2)
-                result = (matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0]);
+                result = Math.Round((matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0]));
             else if (matrix.GetLength(0) == 3 && matrix.GetLength(1) == 3)
-                result = (matrix[0, 0] * ((matrix[1, 1] * matrix[2, 2]) - (matrix[1, 2] * matrix[2, 1]))) - (matrix[0, 1] * ((matrix[1, 0] * matrix[2, 2]) - (matrix[1, 2] * matrix[2, 0]))) + (matrix[0, 2] * ((matrix[1, 0] * matrix[2, 1]) - (matrix[1, 1] * matrix[2, 0])));
+                result = Math.Round((matrix[0, 0] * ((matrix[1, 1] * matrix[2, 2]) - (matrix[1, 2] * matrix[2, 1]))) - (matrix[0, 1] * ((matrix[1, 0] * matrix[2, 2]) - (matrix[1, 2] * matrix[2, 0]))) + (matrix[0, 2] * ((matrix[1, 0] * matrix[2, 1]) - (matrix[1, 1] * matrix[2, 0]))));
             return result;
         }
         return -1;
