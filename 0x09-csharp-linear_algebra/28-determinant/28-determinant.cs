727b6a4bc71cb70c1df54be6ad80656f604dee0a
andzapata <andzapata514@gmail.com>
2020-05-29 19:48:42 -0500

    [Add] determinant of a 2D and 3D matrix

---
+++ 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class MatrixMath
+{
+    public static double Determinant(double[,] matrix)
+    {
+        double[,] fail = new double[,] { { -1 } };
+        double result = 0;
+        if ((matrix.GetLength(0) == 3 && matrix.GetLength(1) == 3) || (matrix.GetLength(0) < 2 && matrix.GetLength(1) < 2))
+        {
+            if (matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2)
+                result = (matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0]);
+            else if (matrix.GetLength(0) == 3 && matrix.GetLength(1) == 3)
+                result = (matrix[0, 0] * ((matrix[1, 1] * matrix[2, 2]) - (matrix[1, 2] * matrix[2, 1]))) - (matrix[0, 1] * ((matrix[1, 0] * matrix[2, 2]) - (matrix[1, 2] * matrix[2, 0]))) + (matrix[0, 2] * ((matrix[1, 0] * matrix[2, 1]) - (matrix[1, 1] * matrix[2, 0])));
+            return result;
+        }
+        return fail;
+    }
+}
