d7811cc771fb9b0ce39f1bf3cd03e698c1d5d752
andzapata <andzapata514@gmail.com>
2020-07-27 16:40:35 -0500

    [Add] fix identation

--- 0x0A-csharp-generics/2-dequeue/2-dequeue.cs
+++ 0x0A-csharp-generics/2-dequeue/2-dequeue.cs
@@ -14,4 +14,43 @@ class Queue<T>
     {
         return typeof(T);
     }
+    /// <summary>
+    /// Class Node inside queue class
+    /// </summary>
+    public class Node
+    {
+        T value;
+        public Node next = null;
+        public Node(T value)
+        {
+            this.value = value;
+        }
+    }
+    public Node head;
+    public Node tail;
+    int count = 0;
+
+    /// <summary>
+    /// Adds a  new node to the end of the queue.
+    /// </summary>
+    /// <param name="value">The value to be added in a new node</param>
+    public void Enqueue(T value)
+    {
+        Node node = new Node(value);
+
+        if (this.head == null)
+            this.head = node;
+        if (this.tail != null)
+            this.tail.next = node;
+        this.tail = node;
+        this.count++;
+    }
+
+    /// <summary>
+    /// Returns the number of nodes in a queue.
+    /// </summary>
+    public int Count()
+    {
+        return this.count;
+    }
 }
