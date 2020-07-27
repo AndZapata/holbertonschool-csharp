4d24f0033d4988f65f1c07f016d03643ae282730
andzapata <andzapata514@gmail.com>
2020-07-27 17:18:01 -0500

    [Fix] queue.cs file in task 1

---
+++ 0x0A-csharp-generics/1-enqueue/queue.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+/// <summary>
+/// Clase Queue Does not inherit from anything
+/// </summary>
+/// <typeparam name="T">param type</typeparam>
+class Queue<T>
+{
+    /// <summary>
+    /// Function to check the type of a param
+    /// </summary>
+    /// <returns>The type of the param</returns>
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+
+    /// <summary>
+    /// Class Node inside queue class
+    /// </summary>
+    public class Node
+    {
+        T value;
+        public Node next = null;
+        public Node(T val)
+        {
+        this.value = val;
+        }
+    }
+    public Node head;
+    public Node tail;
+    int count = 0;
+
+    /// <summary>   
+    /// Adds a  new node to the end of the queue.
+    /// </summary>
+    /// <param name="val">The value to be added in a new node</param>
+    public void Enqueue(T val)
+    {
+        Node newNode = new Node(val);
+
+        if (this.head == null)
+        this.head = newNode;
+        if (this.tail != null)
+        this.tail.next = newNode;
+        this.tail = newNode;
+        this.count++;
+    }
+
+    /// <summary>
+    /// Returns the number of nodes in a queue.
+    /// </summary>
+    public int Count()
+    {
+        return this.count;
+    }
+}
