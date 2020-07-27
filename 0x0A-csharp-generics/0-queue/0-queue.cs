5572294a197858d087449253cb8454be50e44e5a
andzapata <andzapata514@gmail.com>
2020-07-27 15:54:48 -0500

    Update 0-queue.cs

--- 0x0A-csharp-generics/0-queue/0-queue.cs
+++ 0x0A-csharp-generics/0-queue/0-queue.cs
@@ -6,12 +6,12 @@
 /// <typeparam name="T">param type</typeparam>
 class Queue<T>
 {
-  /// <summary>
-  /// Function to check the type of a param
-  /// </summary>
-  /// <returns>The type of the param</returns>
-  public Type CheckType()
-  {
-    return typeof(T);
-  }
+    /// <summary>
+    /// Function to check the type of a param
+    /// </summary>
+    /// <returns>The type of the param</returns>
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
 }
