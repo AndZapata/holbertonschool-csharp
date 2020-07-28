53753088467be510d1793b858a00c0cfbf3bcb74
andzapata <andzapata514@gmail.com>
2020-07-28 17:06:20 -0500

    [Fix] last state + new line

--- 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -36,7 +36,7 @@ interface ICollectable
     void Collect();
 }
 
-class Door : Base, IInteractive, IBreakable, ICollectable
+class TestObject : Base, IInteractive, IBreakable, ICollectable
 {
     public int durability { get; set; }
     public bool isCollected { get; set; }
