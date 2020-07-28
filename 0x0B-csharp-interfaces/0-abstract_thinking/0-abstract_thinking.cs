fb3934dd5948e98e3f6ac35cb5e17d72f1d5ef6c
andzapata <andzapata514@gmail.com>
2020-07-28 16:37:29 -0500

    [Add] first commit + abstract base clase 0 task

---
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+/// <summary>
+/// Abstract class Base
+/// </summary>
+abstract class Base
+{
+  public string name;
+
+  /// <summary>
+  /// Function ToString override
+  /// </summary>
+  /// <returns> returns a new string with name and its type</returns>
+  public override string ToString() => $"{name} is a {this.GetType()}";
+}
