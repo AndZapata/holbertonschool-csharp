8187d24b06e5d8f68ad544e7c532f9c2c0101956
andzapata <andzapata514@gmail.com>
2020-07-28 17:03:42 -0500

    [Add] Interfaces + testObject class

--- 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -1,12 +1,46 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
 
-namespace _1_user_interface
+/// <summary>
+/// Abstract class Base
+/// </summary>
+public abstract class Base
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+  /// <summary>
+  /// given name of the object
+  /// </summary>
+  public string name;
+
+  /// <summary>
+  /// Function ToString override
+  /// </summary>
+  /// <returns> returns a new string with name and its type</returns>
+  public override string ToString() => $"{name} is a {this.GetType()}";
+}
+
+interface IInteractive
+{
+    void Interact();
+}
+
+interface IBreakable
+{
+    int durability { get; set; }
+    void Break();
+}
+
+interface ICollectable
+{
+    bool isCollected { get; set; }
+    void Collect();
+}
+
+class TestObject : Base, IInteractive, IBreakable, ICollectable
+{
+    public int durability { get; set; }
+    public bool isCollected { get; set; }
+    public void Interact(){}
+    public void Break(){}
+    public void Collect(){}
 }
